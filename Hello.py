751ff58d7344df4bbb8ade7c9defa535a929a098
ad8p7sbfknru <150347751+ad8p7sbfknru@users.noreply.github.com>
2023-11-09 19:02:56 +0000

    modified:   Hello.py

--- Hello.py
+++ Hello.py
@@ -14,19 +14,36 @@
 
 import streamlit as st
 from streamlit.logger import get_logger
+from PIL import Image
 
 LOGGER = get_logger(__name__)
 
 def exec():
     st.write("HI")
 
-def dd(dir):
-    st.write(dir)
+def dd1():
     left_rate = int(st.session_state.d1l)
     right_rate = int(st.session_state.d1r)
     forward_rate = 100 - left_rate - right_rate
     st.session_state.d1f = str(forward_rate)
-    st.write(forward_rate)
+
+def dd2():
+    left_rate = int(st.session_state.d2l)
+    right_rate = int(st.session_state.d2r)
+    forward_rate = 100 - left_rate - right_rate
+    st.session_state.d2f = str(forward_rate)
+
+def dd3():
+    left_rate = int(st.session_state.d3l)
+    right_rate = int(st.session_state.d3r)
+    forward_rate = 100 - left_rate - right_rate
+    st.session_state.d3f = str(forward_rate)
+
+def dd4():
+    left_rate = int(st.session_state.d4l)
+    right_rate = int(st.session_state.d4r)
+    forward_rate = 100 - left_rate - right_rate
+    st.session_state.d4f = str(forward_rate)
 
 def run():
     st.set_page_config(
@@ -36,6 +53,13 @@ def run():
 
 
     st.write("# Welcome to CTraf Demo 🚦")
+
+    image = Image.open('crossroad.png')
+
+    st.image(image, caption='X-shaped crossroad')
+
+
+    st.divider()
 
     st.write("Please enter Delta:")
 
@@ -52,9 +76,6 @@ def run():
     
     st.divider()
 
-
-
-
     if "d1f" not in st.session_state:
       st.session_state.d1f = str(70)
     if "d1l" not in st.session_state:
@@ -62,8 +83,6 @@ def run():
     if "d1r" not in st.session_state:
       st.session_state.d1r = 15
     
-
-
     st.subheader("Direction 1")
     
     col1, col2, col3 = st.columns(3)
@@ -71,34 +90,122 @@ def run():
     with col1:
       st.subheader("⬅️")
       st.write("Percentage of vehicles making left turn")
-      st.slider("", min_value=0, max_value=49, value=15, key="d1l", on_change=dd(1))
-
+      st.slider("", min_value=0, max_value=49, value=15, key="d1l", on_change=dd1)
 
     with col2:
       st.subheader("⬆️")
       st.write("Percentage of vehicles going forward")
       placeholder = st.empty()
-      d1f_val = placeholder.text_input("", value=70, max_chars=None, key="d1f", disabled=False)
-
+      d1f_val = placeholder.text_input("", value=70, max_chars=None, key="d1f", disabled=True)
 
     with col3:
       st.subheader("➡️")
       st.write("Percentage of vehicles making right turn")
-      st.slider("", min_value=0, max_value=49, value=15, key="d1r", on_change=dd(1))
+      st.slider("", min_value=0, max_value=49, value=15, key="d1r", on_change=dd1)
+
+    st.divider()
+
+
+    if "d2f" not in st.session_state:
+      st.session_state.d2f = str(70)
+    if "d2l" not in st.session_state:
+      st.session_state.d2l = 15
+    if "d2r" not in st.session_state:
+      st.session_state.d2r = 15
+    
+    st.subheader("Direction 2")
+    
+    col1, col2, col3 = st.columns(3)
+
+    with col1:
+      st.subheader("⬅️")
+      st.write("Percentage of vehicles making left turn")
+      st.slider("", min_value=0, max_value=49, value=15, key="d2l", on_change=dd2)
+
+    with col2:
+      st.subheader("⬆️")
+      st.write("Percentage of vehicles going forward")
+      placeholder = st.empty()
+      d2f_val = placeholder.text_input("", value=70, max_chars=None, key="d2f", disabled=True)
+
+    with col3:
+      st.subheader("➡️")
+      st.write("Percentage of vehicles making right turn")
+      st.slider("", min_value=0, max_value=49, value=15, key="d2r", on_change=dd2)
+
+    st.divider()
+
+
+    if "d3f" not in st.session_state:
+      st.session_state.d3f = str(70)
+    if "d3l" not in st.session_state:
+      st.session_state.d3l = 15
+    if "d3r" not in st.session_state:
+      st.session_state.d3r = 15
+    
+    st.subheader("Direction 3")
+    
+    col1, col2, col3 = st.columns(3)
+
+    with col1:
+      st.subheader("⬅️")
+      st.write("Percentage of vehicles making left turn")
+      st.slider("", min_value=0, max_value=49, value=15, key="d3l", on_change=dd3)
+
+    with col2:
+      st.subheader("⬆️")
+      st.write("Percentage of vehicles going forward")
+      placeholder = st.empty()
+      d3f_val = placeholder.text_input("", value=70, max_chars=None, key="d3f", disabled=True)
+
+    with col3:
+      st.subheader("➡️")
+      st.write("Percentage of vehicles making right turn")
+      st.slider("", min_value=0, max_value=49, value=15, key="d3r", on_change=dd3)
+
+    st.divider()
+
+
+    if "d4f" not in st.session_state:
+      st.session_state.d4f = str(70)
+    if "d4l" not in st.session_state:
+      st.session_state.d4l = 15
+    if "d4r" not in st.session_state:
+      st.session_state.d4r = 15
+    
+    st.subheader("Direction 4")
+    
+    col1, col2, col3 = st.columns(3)
+
+    with col1:
+      st.subheader("⬅️")
+      st.write("Percentage of vehicles making left turn")
+      st.slider("", min_value=0, max_value=49, value=15, key="d4l", on_change=dd4)
+
+    with col2:
+      st.subheader("⬆️")
+      st.write("Percentage of vehicles going forward")
+      placeholder = st.empty()
+      d4f_val = placeholder.text_input("", value=70, max_chars=None, key="d4f", disabled=True)
+
+    with col3:
+      st.subheader("➡️")
+      st.write("Percentage of vehicles making right turn")
+      st.slider("", min_value=0, max_value=49, value=15, key="d4r", on_change=dd4)
 
     st.divider()
 
 
 
-
-    st.write(d1f_val)
-
-
+    #st.write(d1f_val)
+    #st.write(d2f_val)
+    #st.write(d3f_val)
+    #st.write(d4f_val)
 
     st.button("Compute 🚦", "b_exec", None, on_click=exec, args=None)
 
-    if st.button('Reset  🚗'):
-      st.session_state.d1f = str(70)
+    #if st.button('Reset  🚗'):
+    #  st.session_state.d1f = str(70)
 
 
 

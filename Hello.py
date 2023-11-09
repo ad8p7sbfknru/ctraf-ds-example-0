4abf12d0dff54f735a9b45aa2f3c71bf362a0729
ad8p7sbfknru <150347751+ad8p7sbfknru@users.noreply.github.com>
2023-11-09 17:35:16 +0000

    modified:   Hello.py

--- Hello.py
+++ Hello.py
@@ -17,6 +17,15 @@ from streamlit.logger import get_logger
 
 LOGGER = get_logger(__name__)
 
+def exec():
+    st.write("HI")
+
+def dd():
+    left_rate = int(st.session_state.d1l)
+    right_rate = int(st.session_state.d1r)
+    forward_rate = 100 - left_rate - right_rate
+    st.session_state.d1f = str(forward_rate)
+    st.write(forward_rate)
 
 def run():
     st.set_page_config(
@@ -24,7 +33,57 @@ def run():
         page_icon="🚦",
     )
 
+
     st.write("# Welcome to CTraf Demo 🚦")
+
+    st.write("Please enter Delta:")
+
+    d1 = st.number_input("d1", 0, 9999, 100, 1, None, "delta1", "Do Something", on_change=None)
+    d2 = st.number_input("d2", 0, 9999, 100, 1, None, "delta2", "Do Something", on_change=None)
+    d3 = st.number_input("d3", 0, 9999, 100, 1, None, "delta3", "Do Something", on_change=None)
+    d4 = st.number_input("d4", 0, 9999, 100, 1, None, "delta4", "Do Something", on_change=None)
+    
+    
+
+    st.button("Compute 🚦", "b_exec", None, on_click=exec, args=None)
+
+
+    if "d1f" not in st.session_state:
+      st.session_state.d1f = str(70)
+    if "d1l" not in st.session_state:
+      st.session_state.d1l = 15
+    if "d1r" not in st.session_state:
+      st.session_state.d1r = 15
+    
+
+    if st.button('Reset  🚗'):
+      st.session_state.d1f = str(70)
+
+    
+    col1, col2, col3 = st.columns(3)
+
+    with col1:
+      st.header("Left turn")
+      st.slider("Percentage of vehicles making left turn", min_value=0, max_value=49, value=15, key="d1l", on_change=dd)
+
+
+    with col2:
+      st.header("Forward")
+      st.write("Percentage of vehicles going forward")
+      placeholder = st.empty()
+      d1f_val = placeholder.text_input("", value=70, max_chars=None, key="d1f", disabled=False)
+
+      
+    with col3:
+      st.header("Right turn")
+      st.slider("Percentage of vehicles making right turn", min_value=0, max_value=49, value=15, key="d1r", on_change=dd)
+
+
+
+    st.write(d1f_val)
+
+
+    
 
     st.sidebar.success("Select a demo above.")
 
